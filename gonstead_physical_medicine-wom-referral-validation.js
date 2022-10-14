3bc7896f90e472efba03c029ee983417e8d560a3
GetSpanishPatients <113376119+GetSpanishPatients@users.noreply.github.com>
2022-10-14 12:38:11 -0500

    Update gonstead_physical_medicine-wom-referral-validation.js

--- gonstead_physical_medicine-wom-referral-validation.js
+++ gonstead_physical_medicine-wom-referral-validation.js
@@ -47,15 +47,17 @@
 	// + --------------------------------------------------------------------------- +
 	function lookupUser(str) {
 		let userData = formatFormData(str); // fullname, phone, email & referralCode
-		let result = 'success';
+		let result = 'failure';
 		let referal = 'none';
 		for(let i = 0, n = data.length; i < n; i++){ // for has way better performance and since I'm working with a huge chunk of data, why not. Also, call data.length just once.
-			if(data[i].phonenumber  ==  userData[1][1] || data[i].email ==  userData[2][1]) { result = "failure"; return [result, referal]; }
+			if(data[i].phonenumber  ==  userData[1][1] || data[i].email ==  userData[2][1]) return [result, referal];
 			if(data[i].code == userData[3][1]) {
 				referal = "{\"Referer Name\": \"" +data[i].fullname +"\", "
 										+ "\"Referer Phone\": \"" +data[i].phonenumber+"\" , "
 										+ "\"Referer Email\": \"" +data[i].email+"\" , "
 										+ "\"Referer Credit\": \"" +data[i].credit+"\"}";
+				result = 'success';
+				return [result, referal];
 			}
 		};
 		return [result, referal];
