58b904539c6b67d172478b9b6a2a60e150cb3eba
GetSpanishPatients <113376119+GetSpanishPatients@users.noreply.github.com>
2022-10-14 12:40:10 -0500

    Update gonstead_physical_medicine-wom-referral-validation.js

--- gonstead_physical_medicine-wom-referral-validation.js
+++ gonstead_physical_medicine-wom-referral-validation.js
@@ -50,7 +50,7 @@
 		let result = 'failure';
 		let referal = 'none';
 		for(let i = 0, n = data.length; i < n; i++){ // for has way better performance and since I'm working with a huge chunk of data, why not. Also, call data.length just once.
-			if(data[i].phonenumber  ==  userData[1][1] || data[i].email ==  userData[2][1]) return [result, referal];
+			if(data[i].phonenumber  ==  userData[1][1] || data[i].email ==  userData[2][1]) {result = 'failure'; return [result, referal];}
 			if(data[i].code == userData[3][1]) {
 				referal = "{\"Referer Name\": \"" +data[i].fullname +"\", "
 										+ "\"Referer Phone\": \"" +data[i].phonenumber+"\" , "
