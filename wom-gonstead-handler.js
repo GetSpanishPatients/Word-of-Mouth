201842bc878dfbb98df864f593ae899e4ef5d68d
GetSpanishPatients <113376119+GetSpanishPatients@users.noreply.github.com>
2022-10-26 11:12:27 -0500

    Update wom-gonstead-handler.js

--- wom-gonstead-handler.js
+++ wom-gonstead-handler.js
@@ -1,8 +1,5 @@
 'use strict'
 
-  //Strict use for handling users claiming their credits.
-  //Incorrect/Lack of use of this application will result in unseen and incorrect behaviour. We are not responsible for user's mistakes.
-
 	// + --------------------------------------------------------------------------- +
 	// #region Webhook
 	// + --------------------------------------------------------------------------- +
@@ -12,23 +9,35 @@
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		let result = lookupUser($("#myForm").serialize());
-		if(result[0] == 'failure') {
-			authFailure();
-		} else {
-			$.ajax({
-				url: "https://hooks.zapier.com/hooks/catch/3720110/b06hp8r/",
-				type: "post",
-				data: $("#myForm").serialize() + "&url=" + encodeURIComponent(url) + "&refererName=" + encodeURIComponent(JSON.parse(result[1])['Referer Name'])
-																			 + "&refererPhone=" + encodeURIComponent(JSON.parse(result[1])['Referer Phone'])
-																			 + "&refererEmail=" + encodeURIComponent(JSON.parse(result[1])['Referer Email'])
-																			 + "&postReductionCredit=" + encodeURIComponent(JSON.parse(result[1])['Credit Post Reduction'])
-																			 + "&excelRow=" + encodeURIComponent(JSON.parse(result[1])['Excel Row'])
-																			 + "&referralsList=" + encodeURIComponent(JSON.parse(result[1])['Referrals List']),
-				success: function() {
-					authSuccess();
-					//window.location = "http://google.com";
-				}
-			});
+		switch(result[0]){
+			case 'failure':
+				authFailure();
+				break;
+			case 'redeemed':
+				authFailureRedeemed();
+				break;
+			case 'not found':
+				authFailureNotFound();
+				break;
+			case 'success':
+				$.ajax({
+					url: "https://hooks.zapier.com/hooks/catch/3720110/b06hp8r/",
+					type: "post",
+					data: $("#myForm").serialize() + "&url=" + encodeURIComponent(url) + "&refererName=" + encodeURIComponent(JSON.parse(result[1])['Referer Name'])
+																				+ "&refererPhone=" + encodeURIComponent(JSON.parse(result[1])['Referer Phone'])
+																				+ "&refererEmail=" + encodeURIComponent(JSON.parse(result[1])['Referer Email'])
+																				+ "&postReductionCredit=" + encodeURIComponent(JSON.parse(result[1])['Credit Post Reduction'])
+																				+ "&excelRow=" + encodeURIComponent(JSON.parse(result[1])['Excel Row'])
+																				+ "&referralsList=" + encodeURIComponent(JSON.parse(result[1])['Referrals List']),
+					success: function() {
+						authSuccess();
+						//window.location = "http://google.com";
+						}
+					});
+				break;
+			default:
+					alert("Something went wrong!");
+				break;
 		};
 	 });  
 	// #endregion
@@ -51,24 +60,38 @@
 	// #region Authentication
 	// + --------------------------------------------------------------------------- +
 	function lookupUser(str) {
-		let userData = formatFormData(str); // fullname, phone, email & referralCode
+		let userData = formatFormData(str); // fullname, phone
 		let result = 'failure';
 		let referal = 'none';
 		let creditClaimAmmount = 10 // DEVELOPER NOTE: Reduce the necessary credit manually here. ALWAYS update the code when the
 																// clinic updates their offer.
 		for(let i = 0, n = data.length; i < n; i++){
-			if(data[i].phonenumber  ==  userData[1][1])	{
-				let credit = data[i].credit - creditClaimAmmount; 
+			if (data[i].phonenumber  ==  userData[1][1] && !isRedeemContactValid(data[i].referrals)) {
+				result = 'not found';
+				return [result, referal];
+			}
+
+			if(data[i].phonenumber  ==  userData[1][1] && isCreditUnclaimed(data[i].referrals))	{
+				let credit = data[i].credit - creditClaimAmmount;
+				let referralList = stringifyReferralData(data[i].referrals);
+
 				referal = "{\"Referer Name\": \"" +data[i].fullname +"\", "
 				+ "\"Referer Phone\": \"" +data[i].phonenumber+"\" , "
 				+ "\"Referer Email\": \"" +data[i].email+"\" , "
 				+ "\"Credit Post Reduction\": \"" +credit+"\" , "
 				+ "\"Excel Row\": \"" +data[i].row+"\" , "
-				+ "\"Referrals List\": \"" +data[i].referrals+"\"}";
+				+ "\"Referrals List\": \"" +referralList+"\"}";
 				result = "success";
+
+				return [result, referal];
+			} 
+			
+			if (data[i].phonenumber  ==  userData[1][1] && !isCreditUnclaimed(data[i].referrals)) {
+				result = 'redeemed';
 				return [result, referal];
 			}
 		};
+
 		return [result, referal];
 	};
 
@@ -82,8 +105,92 @@
 
 	function authFailure() {
 		let warningDiv = document.querySelector('.u-warning');
+		let warningDivRedeemed = document.querySelector('.u-warning_redeem');
+		let warningDiv404 = document.querySelector('.u-warning_404');
 		warningDiv.classList.add('u-warning-active');
+		warningDivRedeemed.classList.remove('u-warning_redeem-active');
+		warningDiv404.classList.remove('u-warning_404-active');
 	};
+
+	function authFailureRedeemed() {
+		let warningDivRedeemed = document.querySelector('.u-warning_redeem');
+		let warningDiv = document.querySelector('.u-warning');
+		let warningDiv404 = document.querySelector('.u-warning_404');
+		warningDivRedeemed.classList.add('u-warning_redeem-active');
+		warningDiv.classList.remove('u-warning-active');
+		warningDiv404.classList.remove('u-warning_404-active');
+	};
+
+	function authFailureNotFound() {
+		let warningDiv404 = document.querySelector('.u-warning_404');
+		let warningDivRedeemed = document.querySelector('.u-warning_redeem');
+		let warningDiv = document.querySelector('.u-warning');
+		warningDiv404.classList.add('u-warning_404-active');
+		warningDivRedeemed.classList.remove('u-warning_redeem-active');
+		warningDiv.classList.remove('u-warning-active');
+	};
+
+	// + --------------------------------------------------------------------------- +
+	// #region Excel
+	// + --------------------------------------------------------------------------- +
+	function formatReferralData(db) {
+		const referredData = db; // retrieve data from excel
+		const referrals = referredData.split(','); // create an array from the tags
+		let dataReferred = [];
+	
+		referrals.forEach(function(element) {
+			dataReferred.push(element.split('/'));
+		});
+
+		return dataReferred;
+	};
+
+	function isRedeemContactValid(db) {
+		const referredData = formatReferralData(db); // excel data
+		let referredPhone = $("#myForm").serializeArray()[3].value; //form data
+
+		for(let i = 0, n = referredData.length; i < n; i++) {
+			if (referredData[i][1] == referredPhone) return true;
+		};
+
+		return false;
+	};
+
+	function isCreditUnclaimed(db) {
+		const referredData = formatReferralData(db); // excel data
+		let referredPhone = $("#myForm").serializeArray()[3].value; //form data
+
+		for(let i = 0, n = referredData.length; i < n; i++) {
+			if (referredData[i][1] == referredPhone && referredData[i][2] === '(unclaimed)') return true;
+		};
+
+		return false;
+	};
+
+	function stringifyReferralData(array) {
+		const referredData = findRedeemed(array);
+		let items = [];
+
+		referredData.forEach((element) => {
+			items.push(element.join('/'));
+		});
+
+		return items.join(',');
+	};
+
+	function findRedeemed(db) {
+		const referredData = formatReferralData(db); // excel data
+		let referredPhone = $("#myForm").serializeArray()[3].value; //form data
+
+		for(let i = 0, n = referredData.length; i < n; i++) {
+			if (referredData[i][1] == referredPhone){
+				referredData[i][2] = '(redeemed)';
+				return referredData;
+			};
+		};
+
+		return referredData;
+	}
 	// #endregion
 	// + --------------------------------------------------------------------------- +
 
